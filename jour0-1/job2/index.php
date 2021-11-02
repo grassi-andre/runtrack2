70251511ba51f1348364e9a16467333d04ba9e14
grassi-andre <andre.grassi@laplateforme.io>
2021-11-02 11:56:19 +0100

    job2 finit

---
+++ jour0-1/job2/index.php
@@ -0,0 +1,7 @@
+<?php // J'ouvre le php //
+echo "Hello LaPlateforme !";   // Simple echo pour afficher Hello la plateforme //
+$exo="Hello";  // Je Crée une variable exo pour "Hello" //
+$exo2="!"; // Je crée une variable exo2 pour "!" // 
+$exo3="LaPlateforme";  //Je crée une variable exo3 pour "LaPlateforme"//
+echo '<br/>' . $exo. " " . " ". $exo3 . " " .$exo2; // 3 Variables un pour "Hello" un pour "Laplateforme" et un autre pour le "!" que jai echo dans l'ordre
+?> 
