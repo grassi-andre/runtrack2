03f27b0af9b5f110cbaa79131e136fa2354e090e
grassi-andre <andre.grassi@laplateforme.io>
2021-11-02 17:45:08 +0100

    job1-2 finit

---
+++ jour2/job1/index.php
@@ -0,0 +1,10 @@
+<?php
+$nombre=0;
+    while($nombre<=1337){
+        echo "<br> $nombre <br/>";
+        $nombre++;
+        if($nombre==42){
+        echo"<b><u> $nombre </b></u>";
+    }
+    } 
+    ?>
