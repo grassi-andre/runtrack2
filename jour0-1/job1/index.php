d291dd2340eadcd755c66c8d8a0b0e3e76f716bd
grassi-andre <andre.grassi@laplateforme.io>
2021-11-02 11:36:17 +0100

    job1 finit

---
+++ jour0-1/job1/index.php
@@ -0,0 +1,14 @@
+<?php
+$str="LaPlateforme";
+$str2="Vive";
+$str3="!";
+echo $str;
+echo '<br/>'. $str2. " ". $str. " " . $str3;
+$val=6;
+echo '<br/>' . $val;
+echo '<br/>' . ($val +4) ;
+$myBool=true;
+echo $myBool;
+$myBool=false;
+echo $myBool;
+?>
