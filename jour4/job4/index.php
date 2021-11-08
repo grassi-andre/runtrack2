60d57ec599a2208e2249fd8a7d98f8996e505397
grassi-andre <andre.grassi@laplateforme.io>
2021-11-08 12:22:12 +0100

    job4 finit

---
+++ jour4/job4/index.php
@@ -0,0 +1,47 @@
+<html>
+<body>
+
+<form action="index.php" method="post">
+Name: <input type="text" name="name"><br>
+Age: <input type="text" name="age"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+
+
+
+
+<table border = 1>
+    <thead>
+        <tr>
+            <th>Argument</th>
+            <th>valeurs</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+<td>
+<?php
+$i=0;
+
+
+foreach($_POST as $nb => $infos){
+        echo $nb.'<br/>';
+    }
+    $i++;
+
+?>
+</td>
+
+<td>
+    <?php
+
+$a=0;
+
+foreach($_POST as $nb => $infos){
+    echo $infos.'<br/>';
+}
+$a++;
+    ?>
+</td>
