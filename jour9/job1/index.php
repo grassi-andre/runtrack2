dd9d35aa1f364b553eb997180939a4ef08c50f5b
grassi-andre <andre.grassi@laplateforme.io>
2021-11-12 14:47:07 +0100

    job 1finit

--- jour9/job1/index.php
+++ jour9/job1/index.php
@@ -1,27 +1,76 @@
 <?php
 
-$bdb = mysqli_connect("localhost","root","root","jour8");
-$req = mysqli_query($bdb,"SELECT * FROM etudiant");
-$res = mysqli_fetch_all($req);
+$bdd = mysqli_connect('localhost','root', 'root', 'jour8');
 
 
-foreach($res as $key => $value){
-    echo $value [1], '<br/>';
-}
+$req = mysqli_query($bdd, "SELECT * FROM etudiant");
+
+
+$res= mysqli_fetch_all($req, MYSQLI_ASSOC);
+
+
+echo "<pre>";
 var_dump($res);
+echo "<pre>";
 
 ?>
 
-<table>
+<table border="1"> 
     <thead>
-        <tr>
-            <th> </th>
-        </tr>
+            <?php foreach($res[0] as $key => $value){
+                        echo "<th>$key</th>";
+                    }
+?>    
+        
     </thead>
     <tbody>
         <tr>
-            <td></td>
-            <td></td>
+            <?php foreach($res[0] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?> 
+        </tr>
+        <tr>
+
+            <?php foreach($res[1] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?>
+            </td>
+        </tr>
+
+        <tr>
+
+            <?php foreach($res[2] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?>
+            </td>
+        </tr>
+
+        <tr>
+
+            <?php foreach($res[3] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?>
+            </td>
+        </tr>
+        
+        <tr>
+
+            <?php foreach($res[4] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?>
+            </td>
+        </tr>
+
+        <?php foreach($res[5] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?>
+            </td>
+        </tr>
+
+        <?php foreach($res[6] as $key => $value){
+                        echo "<td>$value</td>";
+                    } ?>
+            </td>
         </tr>
     </tbody>
 </table>
